d986510ed46243e1c895a5e0e9d03fefc6e51daf
Dasom Lim <ektha03@gmail.com>
2017-08-29 13:29:33 +0900

    delete "hello world" add string i/o

--- GitTest/src/GitTest.java
+++ GitTest/src/GitTest.java
@@ -1,5 +1,13 @@
-public class GitTest{
-	public static void main(String args[]){
-		System.out.println("hello world");
-	}
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class GitTest {
+    public static void main(String[] args) throws Exception {
+      Scanner sc=new Scanner(System.in);
+      String a=sc.nextLine();
+      System.out.println(a);
+      
+    }
 }
